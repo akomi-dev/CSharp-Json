27acb418473e12051c072fc56788174c4482cc90
Akomi <87880680+akomi-dev@users.noreply.github.com>
2022-05-31 00:04:52 -0400

    Commit

---
+++ CSharpJson/JsonConv.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace CSharpJson;
+
+public static class JsonConv<ClassType> where ClassType : class
+{
+    /// <summary>
+    /// Serializes an object to JSON
+    /// </summary>
+    public static string Serialize(ClassType instance)
+    {
+        var serializer = new DataContractJsonSerializer(typeof(ClassType));
+        using var stream = new MemoryStream();
+        serializer.WriteObject(stream, instance);
+        return Encoding.Default.GetString(stream.ToArray());
+    }
+
+    /// <summary>
+    /// Deserializes an object from JSON string
+    /// </summary>
+    public static ClassType? Deserialize(string json)
+    {
+        using var stream = new MemoryStream(Encoding.Default.GetBytes(json));
+        var serializer = new DataContractJsonSerializer(typeof(ClassType));
+        return serializer.ReadObject(stream) as ClassType;
+    }
+
+    public static void Write(string serializedJson, string filepath)
+    {
+        File.WriteAllText(filepath, serializedJson);
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Runtime.Serialization;
+using CSharpJson;
+
+namespace Testing;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Json File Example
+
+        string filePath = "C:\\Users\\snowy\\Documents\\GitHub\\CSharp-Json\\Testing\\test.json";
+        dynamic file = File.ReadAllText(filePath);
+        var deserialized = JsonConv<AnimeInfo>.Deserialize(file);
+
+        Console.WriteLine(deserialized?.Name);
+        deserialized.Name = "False";
+        Console.WriteLine(deserialized?.Name);
+        deserialized.Name = "Erased"; // name back to default
+        Console.WriteLine(deserialized?.Author);
+        Console.WriteLine(deserialized?.Genre);
+
+        var serialized = JsonConv<AnimeInfo>.Serialize(deserialized);
+        Console.WriteLine(serialized);
+
+        JsonConv<AnimeInfo>.Write(serialized, filePath);
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+
+        // Class Obj Example
+
+        var animeInfo = new AnimeInfo
+        {
+            Name = "Black Clover",
+            Author = "Yuuki Tabata",
+            Genre = "Action"
+        };
+
+        var _serialized = JsonConv<AnimeInfo>.Serialize(animeInfo);
+
+        var _deserialized = JsonConv<AnimeInfo>.Deserialize(_serialized);
+
+        Console.WriteLine(_serialized);
+        Console.WriteLine();
+        Console.WriteLine(_deserialized?.Name);
+        Console.WriteLine(_deserialized?.Author);
+        Console.WriteLine(_deserialized?.Genre);
+    }
+}
+
+[DataContract]
+internal class AnimeInfo
+{
+    [DataMember]
+    public string? Name { get; set; }
+
+    [DataMember]
+    public string? Author { get; set; }
+
+    [DataMember]
+    public string? Genre { get; set; }
+}
